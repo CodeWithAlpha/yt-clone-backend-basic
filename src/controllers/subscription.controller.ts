28cfacccda53d5bf1856ac4faabb1b2118b1ede1
Sharad Choudhary <srd8650@gmail.com>
2025-06-22 17:29:17 +0530

    add controller for my-subscriber and my-subscribed

--- src/controllers/subscription.controller.ts
+++ src/controllers/subscription.controller.ts
@@ -4,6 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler";
 import { IUserDocument } from "../types/user";
 import { ApiResponse } from "../utils/apiResponse";
 import { User } from "../models/user.model";
+import mongoose from "mongoose";
 
 const subscribeChannel = asyncHandler(async (req, res) => {
   try {
@@ -54,4 +55,96 @@ const subscribeChannel = asyncHandler(async (req, res) => {
   }
 });
 
-export { subscribeChannel };
+const mySubscribers = asyncHandler(async (req, res) => {
+  try {
+    const userId = (req as Request & { user: IUserDocument }).user._id;
+
+    const subscribers = await Subscription.aggregate([
+      {
+        $match: {
+          channel: new mongoose.Types.ObjectId(userId as string),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "subscriber",
+          foreignField: "_id",
+          as: "subscriber",
+          pipeline: [
+            {
+              $project: {
+                _id: 1,
+                username: 1,
+                email: 1,
+                fullname: 1,
+                avatar: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          subscriber: { $arrayElemAt: ["$subscriber", 0] },
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, subscribers, "Subscribers found."));
+  } catch (error: any) {
+    console.warn(error);
+    // Catch any validation or server error
+    return res.status(400).json(new ApiResponse(400, null, error.message));
+  }
+});
+
+const mySubscribed = asyncHandler(async (req, res) => {
+  try {
+    const userId = (req as Request & { user: IUserDocument }).user._id;
+
+    const channel = await Subscription.aggregate([
+      {
+        $match: {
+          subscriber: new mongoose.Types.ObjectId(userId as string),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "channel",
+          foreignField: "_id",
+          as: "channel",
+          pipeline: [
+            {
+              $project: {
+                _id: 1,
+                username: 1,
+                email: 1,
+                fullname: 1,
+                avatar: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          channel: { $arrayElemAt: ["$channel", 0] },
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, channel, "Subscribers found."));
+  } catch (error: any) {
+    console.warn(error);
+    // Catch any validation or server error
+    return res.status(400).json(new ApiResponse(400, null, error.message));
+  }
+});
+
+export { subscribeChannel, mySubscribers, mySubscribed };
--- src/routes/subscription.route.ts
+++ src/routes/subscription.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { subscribeChannel } from "../controllers/subscription.controller";
+import {
+  mySubscribed,
+  mySubscribers,
+  subscribeChannel,
+} from "../controllers/subscription.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 import { logRequest } from "../middlewares/activity.middleware";
 
 const router = Router();
 
+router.route("/my-subscribers").get(verifyJWT, logRequest, mySubscribers);
+router.route("/my-subscribed").get(verifyJWT, logRequest, mySubscribed);
 router.route("/:channelid").get(verifyJWT, logRequest, subscribeChannel);
 
 export default router;
